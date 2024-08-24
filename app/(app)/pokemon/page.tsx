5dcb8540c3f7215b771b89248846e7894d0bd00e
DJ-no1 <cseai2023011@rcciit.org.in>
2024-08-24 19:29:41 +0530

    test2

---
+++ app/(app)/pokemon/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const pokemonPage = () => {
+  return (
+    <div>
+        <div className="mockup-phone border-primary">
+  <div className="camera"></div>
+  <div className="display">
+    <div className="artboard artboard-demo phone-1">wanna talk.</div>
+  </div>
+</div>
+    </div>
+  )
+}
+
+export default pokemonPage
